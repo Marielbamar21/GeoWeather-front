08feec386ae5bf2bf1adbe822c0fbf36235b7bd5
Marielbamar21 <marielbamaldonado20@gmail.com>
2024-01-25 12:09:21 -0400

    update

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 
 //selection of sistem elements
-direccions= (dir)=>{
+directions= (dir)=>{
     switch(dir)
     {
         case 'home': window.location.href = '../index.html';
                     break;
         case 'search': window.location.href = './page/search.html';
+                    break;
+        case 'history':
+                    window.location.href = './page/history.html';
                     break;
     }
     
@@ -66,7 +69,7 @@ data = () =>{
     const rowLocation = document.getElementById('rowLocation');
     const temps = document.getElementById('temps')
     const cards = document.getElementById('cards');
-    const locat = document.getElementById('location');
+    const location = document.getElementById('location');
     const temperature = document.getElementById('temperature');
     const apTemperature = document.getElementById('apTemperature');
     const precipitationProb = document.getElementById('precipitationProb');
@@ -85,37 +88,67 @@ data = () =>{
     const visibilityCard = document.getElementById('visibilityCard');
 
     if( !data ){
-        rowLocation.style.marginTop = "20%"
-        locat.style.fontSize = "3rem"
-        
-        locat.innerHTML = 'Esta ubicacion no existe';
-        locat.style.textAlign = "center"
-        temps.style.display = "none";
-        cards.style.display = "none";
-        return;
+       rowLocation.classList.add("row-unlocation");
+        location.classList.add("p-unlocation");
+        location.innerHTML = 'Esta ubicacion no existe';
+        temps.classList.add("notVisibility");
+        cards.classList.add("notVisibility");
+        return; 
+    }
+
+    else{
+        rowLocation.classList.remove("row-unlocation");
+        location.classList.remove("p-unlocation");
+        temps.classList.remove("notVisibility");
+        cards.classList.remove("notVisibility");
+
     }
     
     
     
-    locat.innerHTML = `${data.location}`;
+    location.innerHTML = `${data.location}`;
 
     
-    temperature.innerHTML = `${Math.trunc(data.temperature)}&deg;C/`;
+    temperature.innerHTML = `${Math.trunc(data.temperature)}&deg;C`;
     
-    apTemperature.innerHTML = `${Math.trunc(data.temperatureApparent)}&deg;C `;
+    apTemperature.innerHTML = `${Math.trunc(data.temperatureApparent)}&deg;C`;
 
 
-    !data.humidity ? humidityCard.style.display ="none" : humidity.innerHTML = `${data.humidity}`;
+    !data.humidity ? humidityCard.style.display ="none" : humidity.innerHTML = `${data.humidity}%`;
     
     !data.precipitationProb ? precipitationProbCard.style.display ="none" : precipitationProb.innerHTML = `${data.precipitationProbability}% `;
    
     !data.rainIntensity ? rainIntensityCard.style.display ="none" :rainIntensity.innerHTML = `${data.rainIntensity}`;
     
-    !data.uvHealthConcern ?uvHealthConcernCard.style.display ="none" : uvHealthConcern.innerHTML = `${data.uvHealthConcern} `;
+    !data.uvHealthConcern ? uvHealthConcernCard.style.display ="none" : uvHealthConcern.innerHTML = `${showUVHeath(data.uvHealthConcern)} `;
 
-    !data.uvHealthConcern ? visibilityCard.style.display ="none" : visibility.innerHTML = `${data.visibility} `;
+   
+    !data.visibility ? visibilityCard.style.display ="none" : visibility.innerHTML = `${data.visibility} km`;
     return;
 }
+ const showUVHeath=(data)=>{
+  console.log(data);
+    if((0 <= data) && (data <=2 )){
+      console.log('1',data)
+      return 'Bajo'
+    }
+    else if((2 < data) && (data <=5)){
+      console.log('2',data)
+      return 'Moderado'
+    }
+    else if((5 < data) && (data <= 7)){
+      console.log('3',data)
+      return 'Alto'
+    }
+    else if( (7< data) && (data <=10)){
+      console.log('4',data)
+      return 'Muy Alto'
+    }
+    else{
+      console.log('5',data)
+      return 'Extremo'
+    }
+ }
 
 eventKey=async(event, page)=>{
 
@@ -124,7 +157,7 @@ eventKey=async(event, page)=>{
             if (event.key === 'Enter') {
             await createUser()
             await search();
-            direccions('search');
+            directions('search');
           }
         }
         break;
@@ -144,18 +177,34 @@ eventKey=async(event, page)=>{
 clickButton =async() =>{
             await createUser()
             await search();
-            direccions('search');
+            directions('search');
     }
 
 
-historial= async() =>{;
+historial= async() =>{
     
-    const data = await axios.get(`http://localhost:3000/GeoWeather/weather/`, { withCredentials: true });
-    direccions('search');
-    const cards = document.getElementById('cards');
-    //const location = document.getElementById('location');
-    location.style.display = "none";
-    cards.style.display = "none";
+    try {
+        const response = await axios.get('http://localhost:3000/GeoWeather/weather/', { withCredentials: true });
+        const data = response.data.data;
+        const tableBody = document.getElementById('tableBody');
+
+        data.forEach(item => {
+          const row = document.createElement('tr');
+          row.innerHTML = `<td>${item.location}</td>
+                           <td>${item.temperature}</td>
+                           <td>${item.temperatureApparent}</td>
+                           <td>${item.humidity}</td>
+                           <td>${item.visibility}</td>
+                           <td>${item.uvHealthConcern}</td>
+                           <td>${item.precipitationProbability}</td>
+                           <td>${item.rainIntensity}</td>`;
+          
+          tableBody.appendChild(row);
+        });
+      } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+      }
+    
 
     
 
